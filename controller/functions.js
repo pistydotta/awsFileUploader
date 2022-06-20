1d79d0b66759907b20be82963c13fbe3ac098f22
pistydotta <pisti_dotta@hotmail.com>
2022-06-20 19:35:37 -0300

    tried downsampling images, not worth

--- controller/functions.js
+++ controller/functions.js
@@ -4,8 +4,6 @@ const fsPromises = fs.promises
 const path = require('path')
 const moment = require('moment');
 const _ = require('lodash');
-const { start } = require('repl');
-
 const s3 = new AWS.S3({
     accessKeyId: process.env.AWS_ACCESS_KEY,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
@@ -27,6 +25,11 @@ const uploadToAWS = async (images) => {
                 // console.log(data)
             },
             function (err) {
+                s3.upload({
+                    Bucket: process.env.AWS_BUCKET_NAME,
+                    Key: 'images/' + o,
+                    Body: fileContent
+                })
                 console.log("Erro ao uploadar a imagem: ")
                 console.log(err)
             }
@@ -38,7 +41,7 @@ const uploadToAWS = async (images) => {
 
 module.exports = {
     uploadImages: async (req, res) => {
-        if(!req.body.imageQnt) return res.send("Your body should have a key named imageQnt representing the number of images to be uploaded")
+        if (!req.body.imageQnt) return res.send("Your body should have a key named imageQnt representing the number of images to be uploaded")
         let images = await fsPromises.readdir(process.env.IMAGE_DIRECTORY)
         let workingImages = images.slice(0, req.body.imageQnt)
         const startTime = await uploadToAWS(workingImages)
@@ -51,9 +54,9 @@ module.exports = {
         console.log(data.slice(0, 10))
         res.send("Testando")
     },
-    
+
     downloadResultsFromS3: async (req, res) => {
-        if(!req.body.dirPath) return res.send("Your body should have a key named dirPath representing path from S3 you want to download")
+        if (!req.body.dirPath) return res.send("Your body should have a key named dirPath representing path from S3 you want to download")
         const dirPath = req.body.dirPath
         // console.log(dirPath)
         const data = await s3.listObjectsV2({
@@ -64,7 +67,7 @@ module.exports = {
         for (const obj of data.Contents) {
             let fileName = obj.Key.split('/')[obj.Key.split('/').length - 1]
             console.log(fileName)
-            let readStream = await s3.getObject({Bucket: process.env.AWS_BUCKET_NAME, Key: obj.Key}).createReadStream()
+            let readStream = await s3.getObject({ Bucket: process.env.AWS_BUCKET_NAME, Key: obj.Key }).createReadStream()
             let writeStream = await fs.createWriteStream(path.join(__dirname, `../results/${fileName}`))
             readStream.pipe(writeStream)
         }
@@ -76,7 +79,7 @@ module.exports = {
         dirPath = './results/'
         const results = await fsPromises.readdir(dirPath)
         for (const file of results) {
-            const data = await fsPromises.readFile(dirPath + file, {encoding: 'utf8'})
+            const data = await fsPromises.readFile(dirPath + file, { encoding: 'utf8' })
             resultsData.push(data)
         }
         totalExecTime = 0
@@ -92,4 +95,4 @@ module.exports = {
         console.log(moment.unix(largestTime).format('DD/MM/YYYY HH:mm:ss SSS'))
         res.send("Analyzing results")
     }
- }
+}
